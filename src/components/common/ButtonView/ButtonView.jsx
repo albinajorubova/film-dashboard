35144ea4c960c527a357fcd6a4cd000bcf79061b
albina <dzorubovaa@gmail.com>
2024-11-05 20:55:56 +0300

    Add filmItem: output from local storage remove buttonView

--- src/components/common/ButtonView/ButtonView.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import s from "./ButtonView.module.scss";
-
-const ButtonView = () => {
-  return <button className={s.button} />;
-};
-
-export default ButtonView;
--- src/components/common/FilmItem/FilmItem.jsx
+++ src/components/common/FilmItem/FilmItem.jsx
@@ -1,27 +1,37 @@
-import React from "react";
+import React, { memo } from "react";
 
 import img from "../../../img/sonnaya.jpg";
 
 import s from "./FilmItem.module.scss";
-import ButtonView from "../ButtonView/ButtonView";
 
-const FilmItem = () => {
+const FilmItem = memo(() => {
+  const filmData = localStorage.getItem("filmData");
+  const films = filmData ? JSON.parse(filmData) : [];
+  console.log(films);
   return (
-    <div className={s.block}>
-      <img src={img} alt={"filmname"} className={s.image} />
-      <div className={s.data}>
-        <p className={s.titile}>
-          title <span className={s.span}>date</span>
-        </p>
-        <p className={s.gener}>
-          gener: <span className={s.span}>text</span>
-        </p>
-      </div>
-      <div className={s.buttonView}>
-        <ButtonView />
-      </div>
-    </div>
+    <>
+      {films.length > 0 ? (
+        films.map((film, index) => (
+          <div className={s.block} key={index}>
+            <img src={img} alt={"filmname"} className={s.image} />
+            <div className={s.data}>
+              <p className={s.title}>
+                {film.Title} <span className={s.span}>{film.Year}</span>
+              </p>
+              <p className={s.gener}>
+                genre: <span className={s.span}>{film.Genre}</span>
+              </p>
+            </div>
+            <div className={s.buttonView}>
+              <button className={s.button} />
+            </div>
+          </div>
+        ))
+      ) : (
+        <p className={s.noFilms}>No available films.</p>
+      )}
+    </>
   );
-};
+});
 
 export default FilmItem;
