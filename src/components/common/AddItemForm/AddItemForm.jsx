531d4cd16403104ae4a6d766791d36d65e1c48e8
albina <dzorubovaa@gmail.com>
2024-11-05 21:03:03 +0300

    Add components && icon arrow

---
+++ src/components/common/AddItemForm/AddItemForm.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import s from "./AddItemForm.module.scss";
+
+const AddItemForm = () => {
+  return (
+    <form action="" className={s.form}>
+      <input type="text" />
+    </form>
+  );
+};
+
+export default AddItemForm;
---
+++ src/components/common/FilmView/FilmView.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+import s from "./Home.module.scss";
+
+import Modal from "../Modal/Modal";
+
+const FilmView = () => {
+  const [modalActive, setModalActive] = useState(false);
+  return <div className={s.block}></div>;
+};
+
+export default FilmView;
