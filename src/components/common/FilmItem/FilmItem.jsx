036087471a039b5b503162ab29f20cccc535b647
albina <dzorubovaa@gmail.com>
2024-11-07 15:51:44 +0300

    Add filmView: add quick view for film item

--- src/components/common/FilmItem/FilmItem.jsx
+++ src/components/common/FilmItem/FilmItem.jsx
@@ -1,17 +1,34 @@
-import React, { memo } from "react";
+import React, { memo, useCallback, useState } from "react";
+import cx from "classnames";
 
 import s from "./FilmItem.module.scss";
+
+import Modal from "../Modal/Modal";
+import FilmView from "../FilmView/FilmView";
 
 const FilmItem = memo(() => {
   const filmData = localStorage.getItem("filmData");
   const films = filmData ? JSON.parse(filmData) : [];
+
+  const [modalActive, setModalActive] = useState(false);
+  const [selectedFilm, setSelectedFilm] = useState(null);
+
+  const openModal = useCallback((film) => {
+    setSelectedFilm(film);
+    setModalActive(true);
+  }, []);
+
+  const closeModal = useCallback(() => {
+    setModalActive(false);
+    setSelectedFilm(null);
+  }, []);
 
   return (
     <>
       {films.length > 0 ? (
         films.map((film, index) => (
           <div className={s.block} key={index}>
-            <img src={film.photo} alt={"film photo"} className={s.image} />
+            <img src={film.photo} alt="film photo" className={s.image} />
 
             <div className={s.data}>
               <p className={s.title}>
@@ -21,13 +38,28 @@ const FilmItem = memo(() => {
                 genre: <span className={s.span}>{film.Genre}</span>
               </p>
             </div>
+
             <div className={s.buttonView}>
-              <button className={s.button} />
+              <button
+                onClick={() => openModal(film)}
+                className={cx(s.button, {
+                  [s.openEye]: selectedFilm === film,
+                })}
+              />
             </div>
           </div>
         ))
       ) : (
         <p className={s.noFilms}>No available films.</p>
+      )}
+
+      {selectedFilm && (
+        <Modal
+          setActive={setModalActive}
+          active={modalActive}
+          closeModal={closeModal}
+          content={<FilmView film={selectedFilm} closeModal={closeModal} />}
+        />
       )}
     </>
   );
--- src/components/common/FilmView/FilmView.jsx
+++ src/components/common/FilmView/FilmView.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 
-import s from "./Home.module.scss";
+import s from "./FilmView.module.scss";
 
-import Modal from "../Modal/Modal";
-
-const FilmView = () => {
-  const [modalActive, setModalActive] = useState(false);
-  return <div className={s.block}></div>;
+const FilmView = ({ film }) => {
+  return (
+    <div className={s.block}>
+      <img src={film.photo} alt={film.title} className={s.img} />
+      <div className={s.text}>
+        {Object.entries(film).map(([key, value]) =>
+          key !== "photo" && typeof value !== "object" ? (
+            <p key={key} className={s.prop}>
+              {key}: <span className={s.value}>{value}</span>
+            </p>
+          ) : null
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FilmView;
